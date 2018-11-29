15ae2bba2e48e618a7441d89848d2360d98e74c6
Brandon James <brandonjames879@gmail.com>
2018-11-29 17:39:34 -0600

    Initial commit

---
+++ src/ClientTwo.java
@@ -0,0 +1,9 @@
+
+public class ClientTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ServerTwo.java
@@ -0,0 +1,9 @@
+
+public class ServerTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
