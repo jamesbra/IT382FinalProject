9a758427887c254058b44e4fe4739ce8a016f4a3
Brandon James <brandonjames879@gmail.com>
2018-11-29 17:40:25 -0600

    Added server skeleton code

--- src/ServerOne.java
+++ src/ServerOne.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -8,22 +9,34 @@ import java.net.Socket;
 
 public class ServerOne {
 
+	
+	/*
+	 * Connect to server as soon as the 
+	 */
+	
+	
 	public static void main(String[] args) throws IOException {
 		final int portNumber = 81;
 		System.out.println("Creating server socket on port " + portNumber);
 		ServerSocket serverSocket = new ServerSocket(portNumber);
+		String host = "otherServer";
 		while (true) {
-			Socket socket = serverSocket.accept();
-			OutputStream os = socket.getOutputStream();
+			Socket clientSocket = serverSocket.accept();
+			Socket socketToServer = new Socket(host, portNumber);
+			
+			BufferedReader brFromServer = new BufferedReader(new InputStreamReader(socketToServer.getInputStream()));
+			
+			DataOutputStream os = new DataOutputStream(clientSocket.getOutputStream());
+			
 			PrintWriter pw = new PrintWriter(os, true);
 			pw.println("What's you name?");
 
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			String str = br.readLine();
 
 			pw.println("Hello, " + str);
 			pw.close();
-			socket.close();
+			clientSocket.close();
 
 			System.out.println("Just said hello to:" + str);
 		}
